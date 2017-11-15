03947ee60f21d8e57d189948a55220b87704b99a
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2017-11-15 13:24:34 +0200

    make Enemy inherit Character

--- js/app.js
+++ js/app.js
@@ -20,26 +20,12 @@ Character.prototype.update = function(dt) {
 
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    Character.call(this,'images/enemy-bug.png');
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+Enemy.prototype = Object.create(Character.prototype); // inherit methods from superclass
+Enemy.prototype.constructor = Enemy; // restore self-reference
+// add more specific methods
 
 // Now write your own player class
 // This class requires an update(), render() and
