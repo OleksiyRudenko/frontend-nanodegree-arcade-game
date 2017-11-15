8e113e06513c395d8497b46dfc865454cd3acda1
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2017-11-16 01:15:18 +0200

    add gameSetting.scene render y adjustment

--- js/settings.js
+++ js/settings.js
@@ -4,6 +4,7 @@ var gameSetting = {
     scene: {
         dimension: {width: 505, height: 606},
         block: {width: 101, height: 83},
+        characterRowDelta: -28,
         numRows: 6,
         numCols: 5,
         bugRows: {from: 1, to: 3},
