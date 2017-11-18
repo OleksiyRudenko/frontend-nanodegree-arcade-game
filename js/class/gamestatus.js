35d66c05d2b2a6381c9cae1f67603842482adf8c
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2017-11-18 22:24:34 +0200

    implement GameStatus.render()

--- js/class/gamestatus.js
+++ js/class/gamestatus.js
@@ -7,7 +7,20 @@ var GameStatus = function(player, lives) {
 
 // Renders status bar and event messages
 GameStatus.prototype.render = function() {
+    // draw box
+    ctx.beginPath();
+    ctx.rect(0,0,gameSetting.scene.dimension.width,50);
+    ctx.fillStyle='#FFFF66';
+    ctx.fill();
 
+    // print text
+    // 'Lives:'
+    ctx.fillStyle='#000066';
+    ctx.font = '24px arial';
+    ctx.fillText("Lives:", 100, 40);
+    ctx.font = 'bold 24px arial';
+    ctx.textAlign = 'right';
+    ctx.fillText(this.lives, 150, 40, 50);
 };
 
 // Called when Player reaches river
